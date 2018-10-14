6974371cf7bd225d8c9c7fc8fabb5eeb07bc48c1
Daniel Villanueva <danielvillanueva.0926@gmail.com>
2018-10-13 21:11:34 -0700

    HAE appengine

--- appengine-standard-java8/helloworld/src/main/java/com/example/appengine/java8/HelloAppEngine.java
+++ appengine-standard-java8/helloworld/src/main/java/com/example/appengine/java8/HelloAppEngine.java
@@ -52,9 +52,9 @@ public class HelloAppEngine extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-System.out.println("fdsiojf;sdljf");
-System.out.println("fdsiojf;sdljf");
-System.out.println("fdsiojf;sdljf");
+        System.out.println("fdsiojf;sdljf");
+        System.out.println("fdsiojf;sdljf");
+        System.out.println("fdsiojf;sdljf");
         final Query fromCloud = new Query("D");
         PreparedQuery grilledCloud = datastore.prepare(fromCloud);
         List<Entity> posts = grilledCloud.asList(FetchOptions.Builder.withLimit(5));
@@ -62,7 +62,6 @@ System.out.println("fdsiojf;sdljf");
       System.out.println("soze "+ posts.size());
         posts.forEach(
           (result) -> {
-            System.out.println("fdsiojf;sdljf");System.out.println("fdsiojf;sdljf");System.out.println("fdsiojf;sdljf");System.out.println("fdsiojf;sdljf");
         // Grab the key and convert it into a string in preparation for encoding
         String keyString = KeyFactory.keyToString(result.getKey());
         // Encode the entity's key with Base64
